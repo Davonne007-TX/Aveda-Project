531c5201a7f64588b8f90a59d7e72109032aa9e7
Vonney <davonnejv@gmail.com>
2023-10-15 17:53:39 -0500

    adding aveda project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import TopHeader from './components/TopHeader'
+
+function App() {
+
+  return (
+    <>
+      <TopHeader />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/TopHeader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "../css/TopHeader.css";
+
+export default function TopHeader() {
+  return (
+    <>
+      <div className="top-header">
+        <p>Find Stores & Salons</p>
+        <p>Join Rewards</p>
+        <p>Sign in</p>
+      </div>
+      <h1>Ava's</h1>
+      <div className="black-background">Last Chance To Receive 50% off </div>
+    </>
+  );
+}
